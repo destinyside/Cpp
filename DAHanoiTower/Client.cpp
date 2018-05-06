d25844d7ed97f4354e403210a17b28f220344317
Destinyside <destinyside@163.com>
2018-05-06 17:40:19 +0800

    command line argument resolve

--- DAHanoiTower/Client.cpp
+++ DAHanoiTower/Client.cpp
@@ -1,19 +1,36 @@
 #include<iostream>
+#include<unistd.h>
+#include<stdlib.h>
 using namespace std;
 void move(int n, char A, char B, char C) {
-	if (n <= 1) {
-		cout << "move : " << A << " --> " << C << endl;
-	}
-	else {
-		move(n - 1, A, C, B);
-		cout << "move : " << A << " --> " << C << endl;
-		move(n - 1, B, A, C);
-	}
+    if (n <= 1) {
+	cout << "move : " << A << " --> " << C << endl;
+    }
+    else {
+	move(n - 1, A, C, B);
+	cout << "move : " << A << " --> " << C << endl;
+	move(n - 1, B, A, C);
+    }
 }
-int main() {
-	char A = 'A';
-	char B = 'B';
-	char C = 'C';
-	move(3, A, B, C);
-	system("pause");
+int main(int argc, char** argv) {
+    int step = 3;
+    int ch = 0;
+    while((ch = getopt(argc, argv, "n:")) != -1){
+	switch(ch){
+	    case 'n' : {
+			   step = atoi(optarg);
+			   cout<< "the step is " << step <<endl;
+			   break;
+		       }
+	    default : {
+			  break;
+		      }
+	}
+    }
+
+    char A = 'A';
+    char B = 'B';
+    char C = 'C';
+    move(step, A, B, C);
+    system("pause");
 }
