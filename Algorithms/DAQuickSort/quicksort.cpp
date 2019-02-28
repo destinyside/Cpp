007828c4037aa9a1444766b2dc91c04fe6ff3c0f
Destinyside <destinyside@163.com>
2019-02-28 22:09:38 +0800

    fix quicksort

--- Algorithms/DAQuickSort/quicksort.cpp
+++ Algorithms/DAQuickSort/quicksort.cpp
@@ -2,7 +2,7 @@
 #include<ctime>
 using namespace std;
 
-void quicksort(int* &p, int left, int right) {
+void quicksort(int* p, int left, int right) {
 	if (left > right) {
 		return;
 	}
@@ -10,26 +10,22 @@ void quicksort(int* &p, int left, int right) {
 	int i = left;
 	int j = right;
 	while (i != j) {
+		while (sign < p[j] && i < j) {
+			j--;
+		}
+		if (j > i) {
+			p[i] = p[j];
+		}
 		while (sign >= p[i] && i < j) {
 			//遇到不满足的数就停止循环，接着进行交换
 			i++;
 		}
-		while (sign < p[j] && i < j) {
-			j--;
-		}
-
-		if (i < j) {
-			int tmp = p[i];
-			p[i] = p[j];
-			p[j] = tmp;
+		if(i < j){
+			p[j]= p[i];
 		}
 
 	}
-
-	int tmp = p[i];
-	p[i] = p[left];
-	p[left] = tmp;
-
+	p[i] = sign;
 	quicksort(p, left, i - 1);
 	quicksort(p, i + 1, right);
 
@@ -46,13 +42,19 @@ int* randomArray(int len) {
 	return tmp;
 }
 
-int main() {
-	int len = 5;
-	int *arr = randomArray(len);
-	quicksort(arr, 0, len-1);
+void printArray(int* arr,int len){
 	for (int i = 0; i < len; i++) {
 		cout << arr[i] << " ";
 	}
+	cout<<endl;
+}
+
+int main() {
+	int len = 15;
+	int *arr = randomArray(len);
+	printArray(arr, len);
+	quicksort(arr, 0, len-1);
+	printArray(arr, len);
 	delete[] arr;
-	system("pause");
+	//system("pause");
 }
